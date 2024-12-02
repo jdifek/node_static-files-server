34e630152152f0a0b47e47f49a8528cb7dc46ec5
jdifek <givotkov11@gmail.com>
2024-12-02 11:43:00 +0200

    fsfd

--- src/createServer.js
+++ src/createServer.js
@@ -4,67 +4,46 @@ const http = require('http');
 
 function createServer() {
   return http.createServer((req, res) => {
-    const reUrl = new URL(req.url, `http://${req.headers.host}`);
+    const { pathname } = new URL(`http://${req.headers.host}${req.url}`);
+    const requestedPath =
+      pathname.replace('/file', '').slice(1) || 'index.html';
+    const realPath = path.join(__dirname, '..', 'public', requestedPath);
 
-    // Проверка на наличие '/file/' в пути
-    if (!reUrl.pathname.startsWith('/file/')) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Пожалуйста, используйте /file/ для запроса файлов.');
-
-      return;
-    }
-
-    // Удаляем '/file/' из пути
-    const requestedPath = reUrl.pathname.replace('/file/', '');
-    const publicDir = path.resolve(__dirname, 'public');
-    const filePath = path.join(publicDir, requestedPath);
-
-    // Проверка на попытку обхода каталога
-    if (!filePath.startsWith(publicDir)) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('Неверный запрос: обход каталога не разрешен');
+    if (pathname.includes('//')) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('To load files, use the /file/ path');
 
       return;
     }
 
-    // Обработка запроса
-    fs.promises
-      .stat(filePath)
-      .then((stats) => {
-        if (!stats.isFile()) {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('Файл не найден');
+    if (pathname === '/file') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('To load files, use the /file/ path');
 
-          return;
-        }
+      return;
+    }
 
-        // Определение типа контента
-        const fileExtension = path.extname(filePath).toLowerCase();
-        let contentType = 'application/octet-stream';
+    if (!pathname.startsWith('/file/')) {
+      res.setHeader('Content-Type', 'text/plain');
+      res.statusCode = 400;
+      res.end('To load files, use the /file/ path');
 
-        if (fileExtension === '.html') {
-          contentType = 'text/html';
-        } else if (fileExtension === '.css') {
-          contentType = 'text/css';
-        } else if (fileExtension === '.js') {
-          contentType = 'application/javascript';
-        }
+      return;
+    }
 
-        res.writeHead(200, { 'Content-Type': contentType });
+    fs.readFile(realPath, (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.end(data);
 
-        const readStream = fs.createReadStream(filePath);
-
-        readStream.pipe(res);
-      })
-      .catch((error) => {
-        if (error.code === 'ENOENT') {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('Файл не найден');
-        } else {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Внутренняя ошибка сервера');
-        }
-      });
+        return;
+      }
+      res.setHeader('Content-Type', 'text/plain');
+      res.statusCode = 404;
+      res.end('File not found');
+    });
   });
 }
 
